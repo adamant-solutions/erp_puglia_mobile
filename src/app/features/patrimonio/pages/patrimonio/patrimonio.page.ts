a342c3a94e63f1ef0ed02a4df995600ed4755e7d
Roanda Daja <roanda.daja@adamant-solutions.com>
2024-11-13 17:02:49 +0100

    patrimonio page design

--- src/app/features/patrimonio/pages/patrimonio/patrimonio.page.ts
+++ src/app/features/patrimonio/pages/patrimonio/patrimonio.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-patrimonio',
@@ -9,12 +10,25 @@ import { ActivatedRoute } from '@angular/router';
 export class PatrimonioPage implements OnInit {
 
   pageTitle: string = "patrimonio";
+  patrimonio = ["patrimonio1", "patrimonio2" ,"patrimonio3","patrimonio4", "patrimonio5" ,"patrimonio6","patrimonio7", "patrimonio8" ,"patrimonio9","patrimonio10", "Test" ,"Ertet","Asdss", "Kastr" ,"Od"];
+  results = [...this.patrimonio];
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     const data = this.route.snapshot.data['patrimonioResolver'];
    // console.log(data)
   }
+  
+  handleInput(event: any) {
+  const searchItem = event.detail.value;
+  this.results = this.patrimonio.filter((d) => d.toLowerCase().indexOf(searchItem.toLowerCase()) > -1);
+  /* console.log(this.results) */
+  }
 
-
+  onIonInfinite(ev: any) {
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
+  
 }
