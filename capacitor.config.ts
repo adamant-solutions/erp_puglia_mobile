3d70372232614b7f3c83ca09bc1a6af717846d25
Roanda Daja <roanda.daja@adamant-solutions.com>
2024-12-20 14:16:19 +0100

    capacitor added & enabled

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
 import type {CapacitorConfig} from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.starter.erp.puglia',
   appName: 'erp-puglia',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
-
 export default config;
