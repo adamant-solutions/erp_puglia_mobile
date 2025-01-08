d21f05ec1c36e56db238f3548c80e3104a1d8cbb
Roanda Daja <roanda.daja@adamant-solutions.com>
2025-01-08 20:31:28 +0100

    comune & provincia data

---
+++ src/app/features/patrimonio/data/provincia.ts
@@ -0,0 +1,31 @@
+export interface Provincia {
+  sigla: string;
+  nome: string;
+}
+
+export const provinciaList: Provincia[] = [
+    {
+      "sigla": "BA",
+      "nome": "Bari"
+    },
+    {
+      "sigla": "BT",
+      "nome": "Barletta-Andria-Trani"
+    },
+    {
+      "sigla": "BR",
+      "nome": "Brindisi"
+    },
+    {
+      "sigla": "FG",
+      "nome": "Foggia"
+    },
+    {
+      "sigla": "LE",
+      "nome": "Lecce"
+    },
+    {
+      "sigla": "TA",
+      "nome": "Taranto"
+    }
+  ]
